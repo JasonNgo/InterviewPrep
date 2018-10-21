cafdc2b91a474a445ff2f854b0ec29304f7d1bdf
Jason Ngo <jason.n.ngo@gmail.com>
2018-10-20 22:52:01 -0400

    finished 1.6

--- Cracking the Coding Interview/Ch1-Arrays-and-Strings/Exercise-1.6.playground/Contents.swift
+++ Cracking the Coding Interview/Ch1-Arrays-and-Strings/Exercise-1.6.playground/Contents.swift
@@ -8,30 +8,49 @@
 // Input: aabbccccaa
 // Output: a2b2c4a2
 
+//
+
 public func compress(string: String) -> String {
     
+    // Create an array to hold each set of repeated characters
+    // we can reserve space for the array since we know the max number of possible entries is equal to
+    // the number of characters in the string.
+    typealias Entry = (character: Character, count: Int)
+    var entries: [Entry] = []
+    entries.reserveCapacity(string.count)
+    
     var currentIndex = string.startIndex
-    var result: String = ""
-    var character: Character
+    var character = string[currentIndex] // init to first character
+    var count = 0                        // number of repeated characters
     
-    character = string[currentIndex]
-    
-    var count = 0
+    // iterate through the string counting the number of repeating characters
     while currentIndex < string.endIndex {
         if string[currentIndex] == character {
             currentIndex = string.index(after: currentIndex)
             count += 1
         } else {
-            result = result + "\(character)" + "\(count)"
+            entries.append(Entry(character: character, count: count))
             count = 0
             character = string[currentIndex]
         }
     }
     
-    result = result + "\(character)" + "\(count)"
+    entries.append(Entry(character: character, count: count))
     
-    return result.count > string.count ? string : result
+    // if the length of the compressed version is longer than the input string, return input string
+    // we multiply the number of entries by 2 since each entry has a character and count associated with it.
+    if (entries.count * 2) > string.count { return string }
+    
+    var result: String = ""
+    for entry in entries {
+        result = result + "\(entry.character)\(entry.count)"
+    }
+    
+    return result
 }
 
 var input = "abcdef"
 compress(string: input)
+
+var input2 = "aabbccccaa"
+compress(string: input2)
